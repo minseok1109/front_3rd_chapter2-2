ab0e72b519b078b444da376ad1ba988f7a7d2050
ms-bang <ms.bang@teamsparta.co>
2024-10-20 14:34:59 +0900

    feat: 🎸 기본 파일 구성

---
+++ src/basic/hooks/useCoupon.ts
@@ -0,0 +1,3 @@
+export const useCoupon = () => {
+  return {};
+};
---
+++ src/basic/hooks/utils/cart.ts
@@ -0,0 +1,7 @@
+export const calculateItemTotal = () => {};
+
+export const getMaxApplicableDiscount = () => {};
+
+export const updateCartItemQuantity = () => {};
+
+export const calculateCartTotal = () => {};
